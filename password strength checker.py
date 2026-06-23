6c0201288e5f5de3659418e552c6786a822673b9
Divya Vij <divyavij1818@gmail.com>
2026-06-23 20:14:25 +0530

    Strength Checker updated

--- password strength checker.py
+++ password strength checker.py
@@ -17,17 +17,19 @@ if ( len(password)>= 8 and has_symbol and has_digit and has_lower and has_upper)
     print("Strong Password")
 else:
     print("Weak password: ")
-if not has_upper:
-    print("Missing uppercase letter")
+    
+    if not has_upper:
+        print("Missing uppercase letter")
 
-if not has_lower:
-    print("Missing lowercase letter")
+    if not has_lower:
+        print("Missing lowercase letter")
 
-if not has_digit:
-    print("Missing digit")
+    if not has_digit:
+        print("Missing digit")
 
-if not has_symbol:
-    print("Missing symbol")
+    if not has_symbol:
+        print("Missing symbol")
 
-if len(password) < 8:
-    print("Password too short")
+    if len(password) < 8:
+        print("Password too short")
+        
